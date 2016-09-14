51291f7056a835af05fc022c56440e9eff762ebd
farid <arid1995@mail.ru>
2016-09-14 17:13:41 +0300

    Fail test case for travis testing

--- test.js
+++ test.js
@@ -9,5 +9,6 @@ assert.equal(filter('КЕК ПЕК'), '*** ***');
 assert.equal(filter('КЕК ПЕК, Shrek is a kek.'), '*** ***, ***** is a ***.');
 assert.equal(filter('shrek must die cause he is a total kek!'),
                     '***** must die cause he is a total ***!');
+assert.equal(filter('КЕК KЕK'), '*** ***')
 //TODO: Кейсы для функции filter
 // assert.equal(filter('КЕК'), '***');
